a305bc6ad09048f190ece3d39e9f8296d94ef180
Gabrieljf217 <gabrieljf97@hotmail.com>
2019-04-14 16:46:53 -0500

    notificaciones push finalizado

---
+++ README.md
@@ -0,0 +1,6 @@
+### This is a basic example of push notifications using firebase Cloud Messaging on ionic 3
+
+![alt text](https://github.com/gabrieljf217/APP-PushNotifications_ionic3/blob/master/src/assets/imgs/pushnoti.png)
+
+![alt text](https://github.com/gabrieljf217/APP-PushNotifications_ionic3/blob/master/src/assets/imgs/notiApp.png)
+
---
+++ src/app/app.component.ts
@@ -0,0 +1,48 @@
+import { Component } from '@angular/core';
+import { Platform } from 'ionic-angular';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+// Implementamos la librería de notificaciones Push.
+import { Push, PushObject, PushOptions } from '@ionic-native/push';
+
+import { HomePage } from '../pages/home/home';
+
+@Component({
+  templateUrl: 'app.html'
+})
+export class MyApp {
+  rootPage:any = HomePage;
+
+  constructor(platform: Platform,
+              statusBar: StatusBar,
+              splashScreen: SplashScreen,
+              private push: Push) {
+  platform.ready().then(() => {
+    statusBar.styleDefault();
+    splashScreen.hide();
+
+    // Llamada a la función push.
+    this.pushSetup();
+  });
+  }
+  pushSetup(){
+    const options: PushOptions = {
+      android: {
+          // Añadimos el sender ID para Android.
+          senderID: '770550794542'
+      },
+      ios: {
+          alert: 'true',
+          badge: true,
+          sound: 'false'
+      }
+    };
+
+    const pushObject: PushObject = this.push.init(options);
+
+    pushObject.on('notification').subscribe((notification: any) => console.log('Received a notification', notification));
+    pushObject.on('registration').subscribe((registration: any) => console.log('Device registered', registration));
+    pushObject.on('error').subscribe(error => console.error('Error with Push plugin', error));
+  }
+}
+
